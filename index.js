f2390942a4664a3c041c5cb112e9d3ff1fbbff7d
Brandi Burnell <brandiburnell6@gmail.com>
2023-10-28 14:18:54 -0600

    passed all tests

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeInfo) {
+    return {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(dateStamp) {
+    // console.log(this);
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number(dateStamp.substring(11,15)),
+        date: dateStamp.substring(0,10)
+    });
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number(dateStamp.substring(11,15)),
+        date: dateStamp.substring(0,10)
+    });
+
+    return this;
+}
+
+function hoursWorkedOnDate(givenDate) {
+    let startingTime = this.timeInEvents.find(e => e.date === givenDate);
+    let endingTime = this.timeOutEvents.find(e => e.date === givenDate);
+    return (endingTime.hour - startingTime.hour)/100;
+}
+
+function wagesEarnedOnDate(givenDate) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, givenDate);
+}
+
+function findEmployeeByFirstName(employeeRecords, firstNameWanted) {
+    return employeeRecords.find(employee => employee.firstName === firstNameWanted);
+}
+
+function calculatePayroll(employeeRecords) {
+    const moneyOwedToEmployees = [];
+    employeeRecords.forEach(employee => {
+        moneyOwedToEmployees.push(allWagesFor.call(employee));
+    });
+    return moneyOwedToEmployees.reduce((accum, currentVal ) => accum + currentVal, 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +77,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
